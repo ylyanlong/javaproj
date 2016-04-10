91987bad97de1c99160bd84ad58ed6f520617b39
yanlong <ylyanlongm@gmail.com>
2016-04-10 21:15:14 +0800

    多线程学习

---
+++ common/src/main/java/com/yl/array/ArrayT1.java
@@ -0,0 +1,13 @@
+package com.yl.array;
+
+/**
+ * Created by Administrator on 2016/4/10.
+ */
+public class ArrayT1 {
+    public static void main(String[] args){
+        String[] strs = new String[2];
+        System.out.println(strs.length);
+
+        String[] str3 = new String[5];
+    }
+}
